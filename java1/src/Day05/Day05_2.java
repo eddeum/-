209921e2e6c8d03f07e58e459618036fd9a72b16
504 <504@DESKTOP-I85C8NH>
2022-03-07 16:20:48 +0900

    Day05

---
+++ java1/src/Day05/Day05_2.java
@@ -0,0 +1,129 @@
+package Day05;
+
+import java.util.Scanner;
+
+public class Day05_2 {
+	
+	// 회원제 방문록 프로그램[2차원 배열]
+		// 1. 초기메뉴[1. 회원가입 2. 로그인]
+			// 1. 회원가입[아이디, 비밀번호, 이름]
+			// 2. 로그인[아이디, 비밀번호 동일하면 로그인처리]
+		// 2. 로그인 시 메뉴[1. 방문록쓰기 2. 로그아웃]
+			// 1. 방문록쓰기[제목, 내용, 작성자(로그인된 아이디)]
+			// 2. 로그아웃[초기메뉴로 돌아가기]
+
+	public static void main(String[] args) {
+		
+		// 준비
+		Scanner scanner = new Scanner(System.in);	// 입력객체
+		// 회원 100명[id, pw, name] 저장하는 배열
+		String[][] memberlist = new String[100][3];	// 100행 3열 => 회원당 1행[1행=id][2행=pw][3행=name]
+		// 방문록 100개[title, contents, writer] 저장하는 배열
+		String[][] boardlist = new String[100][3];	// 게시물당 1행
+		
+		while(true) { // 무한루프[종료조건 : x]
+			
+			System.out.println("---------------------------");
+			System.out.println("-------- 회원제 방문록 --------");
+			System.out.println("---------------------------");
+			System.out.println("1. 회원가입 2. 로그인 선택 : "); int ch = scanner.nextInt();
+			if(ch==1) {
+				System.out.println("-------- 회원가입 페이지 --------");
+				System.out.print("MEMBER ID : ");	String id = scanner.next();
+				System.out.print("MEMBER PASSWORD : ");	String pw = scanner.next();
+				System.out.print("MEMBER NAME : ");	String name = scanner.next();
+				// String 3개 변수를 입력받아 memberlist 2차원 배열에 저장
+					// 1. 빈 공간[정수=0, 객체(문자열)=null]을 찾아서 저장[만약에 빈공간이 없으면 회원이 100명을 초과]
+					// 2. 아이디 중복체크
+				
+				// 아이디 중복체크
+				boolean idcheck = true;
+				for(int i =0; i<memberlist.length; i++) {
+					if(memberlist[i][0]!=null && memberlist[i][0].equals(id)) {
+						// == null : 회원X	!= null : 회원O
+						System.err.println("알림))동일한 아이디가 존재합니다.");
+						idcheck = false; // 회원가입이 불가능하게 idcheck 변수를 false 변경
+						break;
+					} // if e
+				} // for e
+				// 저장부분
+				if(idcheck) { // idcheck가 true인 경우에만 회원가입처리
+					for(int i = 0; i<memberlist.length; i++) {
+						if(memberlist[i][0] == null) { // i번째 행에 id가 없으면 = 빈공간		
+							memberlist[0][0] = id;	memberlist[0][1] = pw;
+							memberlist[0][2] = name; // i번째 각 열에 데이터 저장
+						
+							System.out.println("알림))회원가입이 되었습니다.");
+							break; // 저장했으면 끝[1번만 저장해야하기 때문]
+						} // if e
+					
+					} // for e
+	
+				} // if e	
+				
+			} // if e
+			else if(ch==2) {
+				System.out.println("-------- 로그인 페이지 --------");
+				System.out.print("MEMBER ID : ");	String id = scanner.next();
+				System.out.print("MEMBER PASSWORD : ");	String pw = scanner.next();
+				
+				boolean logincheck = false; // 로그인 성공 초기값
+				// 배열내 데이터가 입력받은 id와 password가 동일하면 로그인처리
+				for(int i = 0; i<memberlist.length; i++) {
+					
+					if(memberlist[i][0] != null && memberlist[i][0].equals(id) &&
+						memberlist[i][1].equals(pw)) {
+						// i번째 행에 id와 password가 입력받은 값과 equals 같으면
+							// null은 객체가 아니기 때문에 equals가 불가능(==을 사용)
+							System.out.println("알림))로그인 성공");
+						
+							logincheck = true;	// 로그인 성공 값 저장
+/////////////////////////////////////////////////////////////////////////////////////////////
+							while(true) { //  로그인시 메뉴 무한루프[종료조건 : 로그아웃]
+								System.out.println("----------------------------");
+								System.out.println("----------- 방문록 -----------");
+								System.out.println("번호\t작성자\t제목\t내용");
+								// 모든 방문록 출력
+								for(int j = 0; j<boardlist.length; j++) {
+									if(boardlist[j][0] != null) { // 게시물이 없는 인덱스는 제외
+										System.out.printf("%d\t%s\t%s\t%s \n" , j, boardlist[j][2], boardlist[j][0], boardlist[j][1]);
+									} // if e
+								} // for e
+								System.out.println("1. 방문록남기기 2. 로그아웃  선택 : ");	int ch2 = scanner.nextInt();
+						
+								if(ch2==1) {
+									System.out.println("----------------------------");
+									System.out.println("title : ");		String title = scanner.next();	// 제목
+									System.out.println("content : ");	String content = scanner.next();// 내용
+									// 배열 내 공백을 찾아서 저장
+									for(int j = 0; j<boardlist.length; j++) {
+										if(boardlist[j][0] == null) {
+											boardlist[j][0] = title;
+											boardlist[j][1] = content;
+											boardlist[j][2] = id;	// 로그인시 사용된 id를 대입
+											System.out.println("알림))방문록이 작성되었습니다.");
+											break;	
+										} // if e
+									} // for e
+								
+								} // if e
+								else if(ch2==2) {
+									System.out.println("알림))로그아웃");
+									break;	//가장 가까운 반복문 탈출
+								} // else if e
+								else {System.err.println("알림))알 수 없는 번호입니다.");}
+							} // while e
+//////////////////////////////////////////////////////////////////////////////////////////////////////						
+					} // if e
+				} // for e
+				
+				// 로그인 실패시
+				if(logincheck == false) System.err.println("알림)회원정보가 없거나 다릅니다.");
+				
+			} // else if e
+			else {System.err.println("알림))알 수 없는 번호입니다.");}
+			
+		} // while e
+		
+	} //me
+} // ce
---
+++ java1/src/Day05/Day05_3.java
@@ -0,0 +1,104 @@
+package Day05;
+
+import java.util.Scanner;
+
+public class Day05_3 {
+	
+	public static void main(String[] args) {
+		
+		// p.182 7번예제
+		int max = 0;	// 최대값 저장하는 변수
+		int[] array = {1, 5, 3, 8, 2};	// 선언 + 초기화
+		
+		for(int i = 0; i<array.length; i++) {
+			//* i는 0부터 배열의 길이(5)까지 1씩증가하면서 실행 반복처리
+			if(max<array[i])	max = array[i];
+			//* 만약 max보다 i번째 인덱스의 값이 더 크면 max에 i번째 값 대입
+		} // for e			
+		System.out.println("max : " + max);
+		
+		
+		// p.182 8번예제
+		int[][] array2 = {
+				{95, 86},
+				{83, 92, 96},
+				{78, 83, 93, 87, 88}
+		}; // int e
+		
+		int sum = 0;
+		double avg = 0.0;
+		
+		int count = 0;	// 점수의 개수[칸 개수]
+		// 2차원 배열내 모든 데이터 호출
+		for(int i = 0; i<array2.length; i++) { //* 행은 0부터 배열의 행길이(3)미만까지 1씩증가
+			for(int j = 0; j<array2[i].length; j++) { //* 열은 0부터 배열의 열길이(2, 3, 5 -> 행마다 다름)미만까지 1씩증가
+				sum += array2[i][j]; // 누적합계
+			} // for e
+			count += array2[i].length;	// 열길이의 누적합계
+		} // for e
+		
+		avg = (double)sum / count;
+		
+		System.out.println("sum : " + sum);
+		System.out.println("avg : " + avg);
+		
+		// p.183 9번예제
+		boolean run = true;	// while문 스위치 변수
+		int studentNum = 0;	// 학생수 변수
+		int[] scores = null;// 점수 배열
+		Scanner scanner = new Scanner(System.in);	// 입력객체
+		
+		// while(true)
+		while(run) { // 무한루프[종료조건 : 5번 입력시 종료]
+			System.out.println("--------------------------------------");
+			System.out.println("1.학생수 2. 점수입력 3.점수리스트 4.분석 5. 종료");
+			System.out.println("--------------------------------------");
+			System.out.println("선택 : ");
+			
+			int selectNo = scanner.nextInt();
+			
+			if(selectNo==1) {	// 학생수 입력받기
+				System.out.print("학생수>");	studentNum = scanner.nextInt();
+				// 입력받은 학생 수 만큼 배열길이 변경
+				scores = new int[studentNum]; // 호출(선언은 int로 시작)
+			} // if e
+			else if(selectNo==2) {	// 학생 점수 입력받기
+				for (int i = 0; i<scores.length; i++) { // 배열내 모든 인덱스만큼 반복처리
+					System.out.printf("scores[%d] : \n", i);	scores[i] = scanner.nextInt(); // 입력받아 i번째 인덱스에 저장
+				} // for e
+			} // else if e
+			else if(selectNo==3) {	// 학생 점수 출력하기
+				for(int i = 0; i<scores.length; i++) { // 배열내 모든 인덱스만큼 반복처리
+					System.out.printf("scores[%d] : %d \n" , i, scores[i]);
+				} // for e
+			} // else if e
+			else if(selectNo==4) {
+				int max2 = 0;
+				int 총합계 = 0;
+				double avg2 = 0.0;
+				
+				for(int i = 0; i<scores.length; i++) {
+					
+					// 최고점수 구하기
+					if(max2 < scores[i])	max2 = scores[i];	// i번째 인덱스에 데이터 대입
+					// 누적합계
+					총합계 += scores[i];
+				} // for e
+				
+				// 평균점수 구하기
+				avg2 = (double)총합계 / scores.length;
+				
+				System.out.println("최고 점수 : " + max2);
+				System.out.println("평균 점수 : " + avg2);
+			} // else if e
+			else if(selectNo==5) {
+				System.out.println("프로그램 종료");
+				run = false; // 반복문 탈출 (혹은 break;로 사용해도 동일한 역할을함)
+			} // else if e
+		} // while e
+		
+		
+		
+	} // me
+
+} // ce

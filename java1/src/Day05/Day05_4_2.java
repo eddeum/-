fe5d8f8b39434e9d121521e172d6ea3e7a6404ca
504 <504@DESKTOP-I85C8NH>
2022-03-07 17:55:34 +0900

    Day05 추가

--- java1/src/Day05/Day05_4_2.java
+++ java1/src/Day05/Day05_4_2.java
@@ -34,9 +34,9 @@ public class Day05_4_2 {
 					for(int i = 0; i<memberlist.length; i++) {
 						if(memberlist[i][0]==null) {
 							memberlist[0][0]=id; memberlist [0][1] = pw;
+							System.out.println("회원가입이 완료되었습니다.");
+							break;
 						} // if e
-						System.out.println("회원가입이 완료되었습니다.");
-						break;
 					} // for e
 				} // if e
 			} // if e	
@@ -54,89 +54,88 @@ public class Day05_4_2 {
 						System.out.println("로그인 성공");
 						logincheck = true;
 					} // if e
-					if(logincheck == false) System.err.println("알림)회원정보가 없거나 다릅니다.");
-					break;
-				} // for e	
-				
-				if(id.equals("admin")) {
-					while(true) {
-						System.out.println("---------- 관리자 메뉴 ----------");
-						System.out.println("1. 도서등록 2. 도서목록 3. 로그아웃");	int 선택3 = scanner.nextInt();
-						if(선택3==1) {
-							System.out.println("도서명 : ");	String 도서 = scanner.next();
-										
-							boolean bookcheck = true;
-							for(int j = 0; j<booklist.length; j++) {
-								if(booklist[j][0] != null && booklist[j][0].equals(도서)) {
-									System.err.println("알림)동일한 책이 존재합니다.");
-									bookcheck = false;
-									break;
+	//////////////////////////////////////////////////////////////////////////				
+					if(id.equals("admin")) {
+						while(true) {
+							System.out.println("---------- 관리자 메뉴 ----------");
+							System.out.println("1. 도서등록 2. 도서목록 3. 로그아웃");	int 선택3 = scanner.nextInt();
+							if(선택3==1) {
+								System.out.println("도서명 : ");	String 도서 = scanner.next();
 											
-								} // if e
-							} // for e
-							
-							if(bookcheck) {
+								boolean bookcheck = true;
 								for(int j = 0; j<booklist.length; j++) {
-									if(booklist[j][0] == null) {
-										booklist[j][0] = 도서;
+									if(booklist[j][0] != null && booklist[j][0].equals(도서)) {
+										System.err.println("알림)동일한 책이 존재합니다.");
+										bookcheck = false;
+										break;
+												
 									} // if e
-									System.out.println("알림)도서가 등록되었습니다.");
+								} // for e
+								
+								if(bookcheck) {
+									for(int j = 0; j<booklist.length; j++) {
+										if(booklist[j][0] == null) {
+											booklist[j][0] = 도서;
+										} // if e
+										System.out.println("알림)도서가 등록되었습니다.");
+										break;
+									} // for e
+								} // if e
+							} // if e
+							else if(선택3==2) {
+								System.out.println("--------- 도서 목록 ---------");
+								System.out.println("번호\t도서명");
+								System.out.println("--------------------------");
+								for(int j =0; j<booklist.length; j++) {
+									if(booklist[j][0] != null) {
+										System.out.printf("%d\t%s\n" ,j , booklist[j][0]);
+										
+									} // if e
+								} // for e
+							}
+							else if(선택3==3) {
+								System.out.println("로그아웃 합니다.");
+								break;
+							}
+							else System.err.println("알림)알 수 없는 번호입니다.");
+						} // while e
+					} // if e
+					
+					
+					else {
+						while(true) {
+							System.out.println("---------------- 도서프로그램 메뉴 ----------------");
+							System.out.println("1. 도서검색 2. 도서목록 3. 도서대여 4. 도서반납 5. 로그아웃");
+							System.out.println("----------------------------------------------");
+							System.out.println("선택 : ");	int 선택2 = scanner.nextInt();
+							
+							if(선택2==1) {
+								System.out.println("도서명 검색 : "); String 도서명 = scanner.next();
+								
+								boolean bookcheck = false;
+								for(int j = 0; j<booklist.length; j++) {
+									if(booklist[j][0] != null && booklist[j][0].equals(도서명)) {
+										System.out.println("도서명 : " + 도서명 );
+										bookcheck = true;
+									} // if e
+									if(bookcheck==false)	System.err.println("알림)해당 도서 찾을 수 없습니다.");
 									break;
 								} // for e
 							} // if e
-						} // if e
-						else if(선택3==2) {
-							System.out.println("--------- 도서 목록 ---------");
-							System.out.println("번호\t도서명");
-							System.out.println("--------------------------");
-							for(int j =0; j<booklist.length; j++) {
-								if(booklist[j][0] != null) {
-									System.out.printf("%d\t%s\n" ,j , booklist[j][0]);
-									
-								} // if e
-							} // for e
-						}
-						else if(선택3==3) {
-							System.out.println("로그아웃 합니다.");
-							break;
-						}
-						else System.err.println("알림)알 수 없는 번호입니다.");
-					} // while e
-				} // if e
-				
-				
-				else {
-					while(true) {
-						System.out.println("---------------- 도서프로그램 메뉴 ----------------");
-						System.out.println("1. 도서검색 2. 도서목록 3. 도서대여 4. 도서반납 5. 로그아웃");
-						System.out.println("----------------------------------------------");
-						System.out.println("선택 : ");	int 선택2 = scanner.nextInt();
-						
-						if(선택2==1) {
-							System.out.println("도서명 검색 : "); String 도서명 = scanner.next();
-							
-							boolean bookcheck = false;
-							for(int j = 0; j<booklist.length; j++) {
-								if(booklist[j][0] != null && booklist[j][0].equals(도서명)) {
-									System.out.println("도서명 : " + 도서명 );
-									bookcheck = true;
-								} // if e
-								if(bookcheck==false)	System.err.println("알림)해당 도서 찾을 수 없습니다.");
-								break;
-							} // for e
-						} // if e
-						else if(선택==2) {}
-						else if(선택==3) {}
-						else if(선택==4) {}
-						else if(선택==5) {}
-						else {
-							System.err.println("알림)알 수 없는 번호입니다.");
-						} // else e
-					} // while e
-				} // else e
-				
-
-				
+							else if(선택==2) {}
+							else if(선택==3) {}
+							else if(선택==4) {}
+							else if(선택==5) {}
+							else {
+								System.err.println("알림)알 수 없는 번호입니다.");
+							} // else e
+						} // while e
+					} // else e
+	//////////////////////////////////////////////////////////////				
+					if(logincheck == false) System.err.println("알림)회원정보가 없거나 다릅니다.");
+					break;
+				} // for e	
+					
 
 			} // else if
 			

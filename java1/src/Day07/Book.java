37610166a1830a1b426ca465c784defdc929cdf9
으뜸 <@DESKTOP-GQ6MCS9>
2022-03-12 23:17:02 +0900

    수정

--- java1/src/Day07/Book.java
+++ java1/src/Day07/Book.java
@@ -59,6 +59,7 @@ public class Book {
 		System.out.println("---------- 도서대여 페이지 ----------");
 		System.out.println("도서 ISBN : ");	String isbn = Day07_5_BookApplication.scanner.next();
 		
+		int i = 0;
 		for(Book temp : Day07_5_BookApplication.booklist) {
 			// * 임시객체 : 배열이름
 				// 배열내 0번인덱스부터 마지막인덱스까지 하나씩 임시 객체 대입
@@ -66,8 +67,8 @@ public class Book {
 			if(temp != null && temp.ISBN.equals(isbn)) {
 				if(temp.brental) {
 					System.out.println("알림))해당도서 대여합니다.");
-					temp.brental = false; // 대여중으로 변경 ////////////////////////수정하기
-					temp.mid = loginid;					////////////////////////수정하기
+					Day07_5_BookApplication.booklist[i].brental = false; // 대여중으로 변경 ////////////////////////수정하기
+					Day07_5_BookApplication.booklist[i].mid = loginid;					////////////////////////수정하기
 					return;
 				} // if e
 				else {
@@ -84,6 +85,8 @@ public class Book {
 		System.out.println("---------- 도서반납 페이지 ----------");
 		도서대여목록(loginid);
 		System.out.println("도서 ISBN : ");	String isbn = Day07_5_BookApplication.scanner.next();
+		
+		int i = 0;
 		for(Book temp : Day07_5_BookApplication.booklist) {
 			if(temp != null && temp.ISBN.equals(isbn)) { // 입력한 isbn이 있으면
 				if(temp.mid.equals(loginid)) { // 대여인Id와 현재 로그인된id 동일하면
@@ -92,8 +95,8 @@ public class Book {
 					} // if e
 					else { // 대여중이면
 						System.out.println("알림))반납 완료!!!!");
-						temp.brental = true;	////////////////////////수정하기
-						temp.mid = null;		////////////////////////수정하기
+						Day07_5_BookApplication.booklist[i].brental = true;	////////////////////////수정하기
+						Day07_5_BookApplication.booklist[i].mid = null;		////////////////////////수정하기
 						return;
 					} // else e
 				} // if e

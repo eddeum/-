dfe3d9ce5c903a618caebd59d9cf4bf47afc2fac
으뜸 <@DESKTOP-GQ6MCS9>
2022-03-15 01:08:11 +0900

    Day09 test

---
+++ java1_test/src/Day09/HanaBank.java
@@ -0,0 +1,10 @@
+package Day09;
+
+public class HanaBank extends Bank{
+
+	public HanaBank(String ano, String password, String owner, int balance) {
+		super(ano, password, owner, balance);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ java1_test/src/Day09/KBBank.java
@@ -0,0 +1,10 @@
+package Day09;
+
+public class KBBank extends Bank {
+
+	public KBBank(String ano, String password, String owner, int balance) {
+		super(ano, password, owner, balance);
+		// TODO Auto-generated constructor stub
+	}
+
+}
